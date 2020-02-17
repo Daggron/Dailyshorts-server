30966d67b12a087c5c3d97a427a2df8933ca0c8a
Daggron <returnofking04@gmail.com>
2020-02-17 23:55:26 +0530

    configured production env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const getData = require('./workers/index');
-
+require('dotenv').config();
 app.use(cors());
 
 getData();
